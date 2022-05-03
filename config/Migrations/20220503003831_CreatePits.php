e141d875c679460df1ef1aad7b297cb3cd9f3169
cecattot <raulcctt@outlook.com>
2022-05-02 21:08:17 -0400

    Update appController

--- config/Migrations/20220503003831_CreatePits.php
+++ config/Migrations/20220503003831_CreatePits.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 use Migrations\AbstractMigration;
 
-class CreateMinisters extends AbstractMigration
+class CreatePits extends AbstractMigration
 {
     /**
      * Change Method.
@@ -14,21 +14,31 @@ class CreateMinisters extends AbstractMigration
      */
     public function change()
     {
-        $table = $this->table('ministers');
+        $table = $this->table('pits');
+        $table->addColumn('caminho', 'string', [
+            'default' => null,
+            'limit' => 250,
+            'null' => false,
+        ]);
         $table->addColumn('ano', 'string', [
             'default' => null,
             'limit' => 4,
             'null' => false,
         ]);
-        $table->addColumn('semestre', 'string', [
-            'default' => null,
-            'limit' => 1,
-            'null' => true,
-        ]);
         $table->addColumn('ativo', 'string', [
             'default' => 'S',
             'limit' => 1,
             'null' => true,
+        ]);
+        $table->addColumn('semestre', 'string', [
+            'default' => null,
+            'limit' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('data', 'string', [
+            'default' => null,
+            'limit' => 10,
+            'null' => false,
         ]);
         $table->addColumn('created', 'datetime', [
             'default' => null,
@@ -38,17 +48,7 @@ class CreateMinisters extends AbstractMigration
             'default' => null,
             'null' => false,
         ]);
-        $table->addColumn('subject_id', 'integer', [
-            'default' => null,
-            'limit' => 11,
-            'null' => false,
-        ]);
-        $table->addColumn('classroom_id', 'integer', [
-            'default' => null,
-            'limit' => 11,
-            'null' => false,
-        ]);
-        $table->addColumn('shift_id', 'integer', [
+        $table->addColumn('archive_id', 'integer', [
             'default' => null,
             'limit' => 11,
             'null' => false,
